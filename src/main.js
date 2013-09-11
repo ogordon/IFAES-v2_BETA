0f4109a8eb80c1fb6476748d204dd6c060f70cc6
ogordon <ogordon@imelius.com>
2013-09-11 12:34:51 +0200

    Change main.js to include pushwoosh registration

--- src/main.js
+++ src/main.js
@@ -54,6 +54,12 @@ function onNotificationGCM(e) {
                     // Your GCM push server needs to know the regID before it can push to this device
                     // here is where you might want to send it the regID for later use.
                     alert('registration id = '+e.regid);
+                    PushWoosh.appCode = "ABF08-7738C";
+                    PushWoosh.register(e.regid, function(data) {
+                         alert("PushWoosh register success: " + JSON.stringify(data));
+                     }, function(errorregistration) {
+                         alert("Couldn't register with PushWoosh" +  errorregistration);
+                     });
                 }
             break;
 
